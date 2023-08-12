140ef9a12efa7d5ba8c40ca4729cf26f1e6ea6ad
donny_444 <104888835+donny444@users.noreply.github.com>
2023-08-12 23:10:15 +0700

    Add files via upload

---
+++ Python 3/0021(unfinished).py
@@ -0,0 +1,16 @@
+text = list(input("Enter Text: "));
+vowels = ["a", "e", "i", "o", "u"];
+encoded = "p";
+
+for i in range(len(text)):
+    for j in range(len(vowels)):
+        if(text[i]==encoded and text[i-1]==vowels[j] and text[i+1]==vowels[j]):
+            text.pop(i-1);
+            if(text[i+1]):
+                text.pop(i+1);
+            else:
+                continue;
+        else:
+            pass;
+
+print(text);
